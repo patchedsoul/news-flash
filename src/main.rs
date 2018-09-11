5a42f99972d11e62b997bb7b424b6a311cf40d96
Jan Lukas Gernert <jangernert@gmail.com>
2018-09-11 02:20:01 +0200

    gtk sample window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate gio;
+extern crate gtk;
+
+use std::env::args;
+use gio::prelude::*;
+use gtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let application = gtk::Application::new("com.gitlab.newsflash", gio::ApplicationFlags::empty())
+        .expect("Initialization failed...");
+
+    application.connect_startup(move |app| {
+        let window = gtk::ApplicationWindow::new(app);
+
+        window.set_title("First GTK+ Program");
+        window.set_border_width(10);
+        window.set_position(gtk::WindowPosition::Center);
+        window.set_default_size(350, 70);
+
+        window.connect_delete_event(move |win, _| {
+            win.destroy();
+            Inhibit(false)
+        });
+
+        let button = gtk::Button::new_with_label("Click me!");
+
+        window.add(&button);
+
+        window.show_all();
+    });
+    application.connect_activate(|_| {});
+
+    application.run(&args().collect::<Vec<_>>());
 }
