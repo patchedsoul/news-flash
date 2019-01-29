a68d64f752a28ba70d9728efe3041b6fb0f44013
Jan Lukas Gernert <jangernert@gmail.com>
2019-01-29 02:39:31 +0100

    layout error list a bit nicer

--- src/error_dialog.rs
+++ src/error_dialog.rs
@@ -31,21 +31,34 @@ impl ErrorDialog {
         let error_dialog : gtk::Window = builder.get_object("error_dialog").ok_or(format_err!("some err3"))?;
 
         for (i, cause) in Fail::iter_chain(error).enumerate() {
-            let mut string = format!("{}: {}", i, cause);
+            let mut string = format!("{}", cause);
             
             if let Some(error) = NewsFlash::parse_error(cause) {
                 string = format!("{} ({})", string, error);
             }
 
-            let label = gtk::Label::new(None);
-            label.set_text(&string);
-            label.set_size_request(0, 30);
-            label.set_ellipsize(pango::EllipsizeMode::End);
-            label.set_xalign(0.0);
-            label.set_margin_start(5);
-            label.set_margin_end(5);
+            let index_label = gtk::Label::new(None);
+            index_label.set_text(&format!("{}", i,));
+            index_label.set_size_request(30, 0);
+
+            let message_label = gtk::Label::new(None);
+            message_label.set_text(&string);
+            message_label.set_ellipsize(pango::EllipsizeMode::End);
+            message_label.set_xalign(0.0);
+
+            let v_separator = gtk::Separator::new(gtk::Orientation::Vertical);
+
+            let h_box = gtk::Box::new(gtk::Orientation::Horizontal, 5);
+            h_box.set_size_request(0, 30);
+            h_box.set_margin_start(5);
+            h_box.set_margin_end(5);
+            h_box.pack_start(&index_label, false, true, 0);
+            h_box.pack_start(&v_separator, false, true, 0);
+            h_box.pack_start(&message_label, false, true, 0);
+            
+            
             let separator = gtk::Separator::new(gtk::Orientation::Horizontal);
-            list_box.pack_start(&label, false, true, 0);
+            list_box.pack_start(&h_box, false, true, 0);
             list_box.pack_start(&separator, false, true, 0);
         }
 
