d35135acbbd5654efa12bdac96ac8ce32f210c20
Jan Lukas Gernert <jangernert@gmail.com>
2019-01-29 21:46:44 +0100

    set up log4rs

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,20 @@ use crate::main_window::MainWindow;
 use rust_embed::RustEmbed;
 use std::env::args;
 use std::str;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::encode::pattern::PatternEncoder;
+use log::LevelFilter;
+use log4rs::config::{
+    Appender,
+    Config,
+    Root
+};
+use log::{
+    warn,
+    info,
+    error,
+    trace,
+};
 
 #[derive(RustEmbed)]
 #[folder = "resources/"]
@@ -26,6 +40,22 @@ struct Resources;
 fn main() {
     let application = Application::new("com.gitlab.newsflash", gio::ApplicationFlags::empty())
         .expect("Initialization failed...");
+
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{d(%Y-%m-%d %H:%M:%S)} - {({l}):5.5} - {f}:{L} - {m}\n")))
+        .build();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .build(Root::builder().appender("stdout").build(LevelFilter::Trace))
+        .unwrap();
+
+    let _handle = log4rs::init_config(config).unwrap();
+
+    info!("some info");
+    warn!("some warning");
+    error!("some error");
+    trace!("some trace");
 
     application.connect_startup(move |_app| {
 
